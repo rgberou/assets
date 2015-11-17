764743bf23cea8d7fa21730dbcd3e8e2d1192ca9
RgBerou <brotark@gmail.com>
2015-11-17 10:28:12 -0800

    added

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@
   var accounts = [{
       name: 'Genaro C. Legaspi',
       post: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut varius nisl sed laoreet gravida. Duis elementum eleifend nibh, eu dictum ligula feugiat et. Proin luctus interdum commodo.",     
-      image: "https://cdn.rawgit.com/rgberou/assets/master/image/profpic.jpg"
+      image: "https://raw.githubusercontent.com/rgberou/assets/master/image/profpic.jpg"
   },{
       name: 'Genaro C. Legaspi',
       post: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut varius nisl sed laoreet gravida. Duis elementum eleifend nibh, eu dictum ligula feugiat et. Proin luctus interdum commodo.",     
